046df80deb762a1a60bc269559af82abd3f43269
Sindels <sindyengelbrecht07@gmail.com>
2021-04-13 13:39:43 +0200

    Initial commit

---
+++ assets/js/JSresume.js
@@ -0,0 +1,15 @@
+function myFunction1() {
+  let popup1 = document.getElementById("btn1");
+  alert("My Competencies:\n 1. I am self-dissaplined\n 2. I learn quickly\n 3. I can work in a team\n 4. I can use HTML5 and CSS well\n 5. I am always on schedule\n 6. I am a caring person\n 7. I have good communication skills\n 8. I am good in administarion");
+}// I have created a function that will be executed when the read more button with the id btn1 is clicked.
+
+function myFunction2() {
+  let popup2 = document.getElementById("btn2");
+  alert("Work Experience:\n Before I continued my studies, I had a full time job. I worked in the administration sector and helped with the clients. I also have experience with stock taking, keeping track of orders and payments. I never got to work late and never received written warnings.");
+}//I have created another funtion for the button with the id btn2, that will execute this alert.
+
+function myFunction3() {
+  let popup3 = document.getElementById("btn3");
+  alert("Education\n I have finished High school at Hugenote in Springs with Achievement levels of 6 and 7.\n I started my Full Stack Web Development studies at Hyperion Development.\n I don't plan to stop learning and developing better skills, so I can become more understanding and prepared for my line of work. ");
+}// And one more funtion with the alert for the last button with the id btn3. 
+
